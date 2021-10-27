6bc760a27080e7fed1b4d3358b7c8d38600b36bb
Sydney Sydneys <sydneyochieng06@gmail.com>
2021-10-27 23:56:03 +0300

    Subway Skater

---
+++ Subway Skater/Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private const int COIN_SCORE_AMOUNT = 5;
+
+    public static GameManager Instance { set; get; }
+
+    public bool IsDead { set; get; }
+
+    private bool isGameStarted = false;
+    private PlayerMotor motor;
+
+    // UI and UI fields
+    public Animator gameCanvas, menuAnim, diamondAnim;
+    public Text scoreText, coinText, modifierText, highscoreText;
+    private float score, coinScore, modifierScore;
+    private int lastScore;
+
+    // Death Menu
+    public Animator deathMenuAnim;
+    public Text deadScoreText, deadCoinText;
+
+    private void Awake()
+    {
+        Instance = this;
+        modifierScore = 1;
+        motor = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>();
+
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+        coinText.text = coinScore.ToString("0");
+        scoreText.text = scoreText.text = score.ToString("0");
+
+        highscoreText.text = PlayerPrefs.GetInt("Highscore").ToString();
+    }
+
+    private void Update()
+    {
+        if(MobileInput.Instance.Tap && !isGameStarted)
+        {
+            isGameStarted = true;
+            motor.StartRunning();
+            FindObjectOfType<GlacierSpawner>().IsScrolling = true;
+            FindObjectOfType<CameraMotor>().IsMoving = true;
+            gameCanvas.SetTrigger("Show");
+            menuAnim.SetTrigger("Hide");
+        }
+        if (isGameStarted && !IsDead)
+        {
+            // Bump the score up
+            score += (Time.deltaTime * modifierScore);
+            if(lastScore != (int)score)
+            {
+                lastScore = (int)score;
+                scoreText.text = score.ToString("0");
+            }
+        }
+    }
+
+    public void GetCoin()
+    {
+        diamondAnim.SetTrigger("Collect");
+        coinScore ++;
+        coinText.text = coinScore.ToString("0");
+        score += COIN_SCORE_AMOUNT;
+        scoreText.text = scoreText.text = score.ToString("0");
+    }
+
+    public void UpdateModifier(float modifierAmount)
+    {
+        modifierScore = 1.0f + modifierAmount;
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+    }
+
+    public void OnPlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void OnDeath()
+    {
+        IsDead = true;
+        FindObjectOfType<GlacierSpawner>().IsScrolling = false;
+        deadScoreText.text = score.ToString("0");
+        deadCoinText.text = coinScore.ToString("0");
+        deathMenuAnim.SetTrigger("Dead");
+        gameCanvas.SetTrigger("Hide");
+
+        //Check for highscore
+        if (score > PlayerPrefs.GetInt("Highscore"))
+        {
+            float s = score;
+            if (s % 1 == 0)
+                s += 1;
+            PlayerPrefs.SetInt("Highscore", (int)s);
+        }
+    }
+}
---
+++ Subway Skater/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private const float LANE_DISTANCE = 2.5f;
+    private const float TURN_SPEED = 0.05f;
+
+    //
+    private bool isRunning = false;
+
+    // Animation
+    private Animator anim;
+
+    // Movement
+    private CharacterController controller;
+    private float jumpForce = 4.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    
+    private int desiredLane = 1; // 0 = Left, 1 = Middle, 2 = Right
+
+    // Speed Modifier
+    private float originalSpeed = 7.0f;
+    private float speed = 7.0f;
+    private float speedIncreaseLastTrick;
+    private float speedIncreaseTime = 2.5f;
+    private float speedIncreaseAmount = 0.1f;
+
+    private void Start()
+    {
+        speed = originalSpeed;
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isRunning)
+            return;
+
+        if(Time.time - speedIncreaseLastTrick > speedIncreaseTime)
+        {
+            speedIncreaseLastTrick = Time.time;
+            speed += speedIncreaseAmount;
+
+            // Change the modifier text
+            GameManager.Instance.UpdateModifier(speed - originalSpeed);
+        }
+
+        // Gather the inputs on which lines we should be
+        if (MobileInput.Instance.SwipeLeft)
+            MoveLane(false);
+        if (MobileInput.Instance.SwipeRight)
+            MoveLane(true);
+
+        // Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * LANE_DISTANCE;
+
+        // Calculating our move delta
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+
+        bool isGrounded = IsGrounded();
+        anim.SetBool("Grounded", isGrounded);
+
+        // Calculate Y
+        if (isGrounded) // if Grounded
+        {
+            verticalVelocity = -0.1f;
+
+            if (MobileInput.Instance.SwipeUp)
+            {
+                // Jump
+                anim.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+            else if (MobileInput.Instance.SwipeDown)
+            {
+                // Slide
+                StartSliding();
+                Invoke("StopSliding", 1.0f);
+            }
+        }
+        else
+        {
+            verticalVelocity -= (gravity * Time.deltaTime);
+
+            // Fast Falling Mechanic
+            if (MobileInput.Instance.SwipeDown)
+            {
+                verticalVelocity = -jumpForce;
+            }
+        }
+
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        // Move the Pengu
+        controller.Move(moveVector * Time.deltaTime);
+
+        // Rotate the Pengu to where he is going
+        Vector3 dir = controller.velocity;
+        if(dir != Vector3.zero)
+        {
+            dir.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+        }
+    }
+
+    private void StartSliding()
+    {
+        anim.SetBool("Sliding", true);
+        controller.height /= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y / 2, controller.center.z);
+    }
+
+    private void StopSliding()
+    {
+        anim.SetBool("Sliding", false);
+        controller.height *= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y * 2, controller.center.z);
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f, controller.bounds.center.z), Vector3.down);
+        Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
+
+        return Physics.Raycast(groundRay, 0.2f + 0.1f);
+    }
+
+    public void StartRunning()
+    {
+        isRunning = true;
+        anim.SetTrigger("StartRunning");
+    }
+
+    private void Crash()
+    {
+        anim.SetTrigger("Death");
+        isRunning = false;
+        GameManager.Instance.OnDeath();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        switch (hit.gameObject.tag)
+        {
+            case "Obstacle":
+                Crash();
+            break;
+        }
+    }
+}
