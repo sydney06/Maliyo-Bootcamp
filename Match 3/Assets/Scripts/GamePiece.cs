e04f08f1340ecbd05bac621256bc63e62c6bf0a7
Sydney Sydneys <sydneyochieng06@gmail.com>
2021-10-27 22:19:47 +0300

    Match 3 and Subway Skater

---
+++ Match 3/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int score;
+
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get { return x; }
+        set
+        {
+            if (IsMovable())
+            {
+                x = value;
+            }
+        }
+    }
+    public int Y
+    {
+        get { return y; }
+        set
+        {
+            if (IsMovable())
+            {
+                y = value;
+            }
+        }
+    }
+
+    private GridController.PieceType type;
+
+    public GridController.PieceType Type
+    {
+        get { return type; }
+    }
+
+    private GridController grid;
+
+    public GridController GridRef
+    {
+        get { return grid; }
+    }
+
+    private MovablePiece movableComponent;
+
+    public MovablePiece MovableComponent
+    {
+        get { return movableComponent; }
+    }
+
+    private ColorPiece colorComponent;
+
+    public ColorPiece ColorComponent
+    {
+        get { return colorComponent; }
+    }
+
+    private ClearablePiece clearableComponent;
+
+    public ClearablePiece ClearableComponent
+    {
+        get { return clearableComponent; }
+    }
+
+    void Awake()
+    {
+        movableComponent = GetComponent<MovablePiece>();
+        colorComponent = GetComponent<ColorPiece>();
+        clearableComponent = GetComponent<ClearablePiece>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init(int _x, int _y, GridController _grid, GridController.PieceType _type)
+    {
+        x = _x;
+        y = _y;
+        grid = _grid;
+        type = _type;
+    }
+
+    private void OnMouseEnter()
+    {
+        grid.EnterPiece(this);
+    }
+
+    private void OnMouseDown()
+    {
+        grid.PressPiece(this);
+    }
+
+    private void OnMouseUp()
+    {
+        grid.ReleasePiece();
+    }
+
+    public bool IsMovable()
+    {
+        return movableComponent != null;
+    }
+
+    public bool IsColored()
+    {
+        return colorComponent != null;
+    }
+
+    public bool IsClearable()
+    {
+        return clearableComponent != null;
+    }
+}
---
+++ Match 3/Assets/Scripts/Level.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public enum LevelType
+    {
+        TIMER,
+        OBSTACLE,
+        MOVES,
+    };
+
+    public GridController grid;
+    public HUD hud;
+
+    public int score1Star;
+    public int score2Star;
+    public int score3Star;
+
+    protected LevelType type;
+    public LevelType Type
+    {
+        get { return type; }
+    }
+
+    protected int currentScore;
+
+    protected bool didWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud.SetScore(currentScore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void GameWin()
+    {
+        grid.GameOver();
+        didWin = true;
+        StartCoroutine(WaitForGridFill());
+    }
+
+    public virtual void GameLose()
+    {
+        grid.GameOver();
+        didWin = false;
+        StartCoroutine(WaitForGridFill());
+    }
+
+    public virtual void OnMove()
+    {
+
+    }
+
+    public virtual void OnPieceCleared(GamePiece piece)
+    {
+        // Update score
+        currentScore += piece.score;
+        hud.SetScore(currentScore);
+    }
+
+    protected virtual IEnumerator WaitForGridFill()
+    {
+        while (grid.IsFilling)
+        {
+            yield return 0;
+        }
+
+        if (didWin)
+        {
+            hud.OnGameWin(currentScore);
+        }
+        else
+        {
+            hud.OnGameLose();
+        }
+    }
+}
